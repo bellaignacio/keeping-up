9073c7239b1b8342f923a12b7f626e18b734291d
Aurora Ignacio <bellaignacio10@gmail.com>
2023-07-22 11:20:26 -0700

    added default values to list styles and image urls

--- app/models/list.py
+++ app/models/list.py
@@ -11,7 +11,6 @@ class List(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     caption = db.Column(db.String(255), nullable=False)
-    image_url = db.Column(db.String(255))
     order = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
