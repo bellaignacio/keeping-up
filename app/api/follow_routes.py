829f1c5cba4d3ba58d873dc31d137c46d165fe2d
Aurora Ignacio <bellaignacio10@gmail.com>
2023-07-24 21:49:09 -0700

    removed Follow model

--- app/api/follow_routes.py
+++ app/api/follow_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import current_user, login_required
-from app.models import Follow
+from app.models import User
 from app.api.auth_routes import validation_errors_to_error_messages
 
 follow_routes = Blueprint('follows', __name__)
