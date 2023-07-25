3815f5c4d305e6d8344bced67dc16282baabab2a
Aurora Ignacio <bellaignacio10@gmail.com>
2023-07-24 20:53:24 -0700

    changed follows to helper table instead of class

--- app/models/user.py
+++ app/models/user.py
@@ -2,8 +2,14 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
-from app.models.follow import Follow
 
+
+follows = db.Table(
+    "follows",
+    db.Model.metadata,
+    db.Column("user_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), primary_key=True),
+    db.Column("following_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), primary_key=True)
+)
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -22,20 +28,27 @@ class User(db.Model, UserMixin):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
+    # followings = db.relationship(
+    #     "User",
+    #     secondary="follows",
+    #     primaryjoin="follows.c.user_id==User.id",
+    #     secondaryjoin="follows.c.following_id==User.id",
+    #     back_populates="followers",
+    # )
+    # followers = db.relationship(
+    #     "User",
+    #     secondary="follows",
+    #     primaryjoin="follows.c.following_id==User.id",
+    #     secondaryjoin="follows.c.user_id==User.id",
+    #     back_populates="followings",
+    # )
     followings = db.relationship(
-        "User",
-        secondary="follows",
-        primaryjoin="follows.c.user_id==User.id",
-        secondaryjoin="follows.c.following_id==User.id",
-        back_populates="followers",
+        'User', lambda: follows,
+        primaryjoin=lambda: User.id == follows.c.user_id,
+        secondaryjoin=lambda: User.id == follows.c.following_id,
+        backref='followers'
     )
-    followers = db.relationship(
-        "User",
-        secondary="follows",
-        primaryjoin="follows.c.following_id==User.id",
-        secondaryjoin="follows.c.user_id==User.id",
-        back_populates="followings",
-    )
+
     lists = db.relationship("List", back_populates="user", cascade="all, delete-orphan")
     comments = db.relationship("Comment", back_populates="user", cascade="all, delete-orphan")
     likes = db.relationship("Like", back_populates="user", cascade="all, delete-orphan")
