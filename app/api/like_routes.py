ad663a25910b1ef08c917669fcef7c7dcf29ee31
Aurora Ignacio <bellaignacio10@gmail.com>
2023-07-24 18:06:26 -0700

    changed login to email or username

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import current_user, login_required
 from app.models import Like
+from app.api.auth_routes import validation_errors_to_error_messages
 
 like_routes = Blueprint('likes', __name__)
