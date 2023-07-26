88bcccb704750568ac499ed620acbd48717c902f
Aurora Ignacio <bellaignacio10@gmail.com>
2023-07-25 21:52:38 -0700

    completed follow routes

--- app/api/follow_routes.py
+++ app/api/follow_routes.py
@@ -1,6 +1,42 @@
 from flask import Blueprint, jsonify
 from flask_login import current_user, login_required
-from app.models import User
+from app.models import db, User
 from app.api.auth_routes import validation_errors_to_error_messages
 
 follow_routes = Blueprint('follows', __name__)
+
+
+@follow_routes.route('/<int:user_id>', methods=['POST'])
+@login_required
+def follow_user(user_id):
+    """
+    Creates a follow
+    """
+    user = User.query.get(user_id)
+    current_user.followings.append(user)
+    db.session.commit()
+    return current_user.to_dict()
+
+
+@follow_routes.route('/<int:user_id>', methods=['DELETE'])
+@login_required
+def unfollow_user(user_id):
+    """
+    Deletes a follow
+    """
+    user = User.query.get(user_id)
+    current_user.followings.remove(user)
+    db.session.commit()
+    return current_user.to_dict()
+
+
+@follow_routes.route('/remove/<int:user_id>', methods=['DELETE'])
+@login_required
+def remove_follower(user_id):
+    """
+    Deletes a follow
+    """
+    user = User.query.get(user_id)
+    current_user.followers.remove(user)
+    db.session.commit()
+    return current_user.to_dict()
