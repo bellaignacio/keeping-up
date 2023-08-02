63c6eb73a1f82066a8eaaf977996fe623273f6bb
Aurora Ignacio <bellaignacio10@gmail.com>
2023-08-01 23:25:06 -0700

    completed edit and delete comment form

--- react-app/src/components/DeleteCommentModal/index.js
+++ react-app/src/components/DeleteCommentModal/index.js
@@ -1,10 +1,28 @@
+import { useDispatch } from 'react-redux';
+import { useHistory } from "react-router-dom";
+import { useModal } from '../../context/Modal';
+import * as listActions from "../../store/list";
 import './DeleteComment.css';
 
-function DeleteCommentModal() {
+function DeleteCommentModal({ commentObj }) {
+    const { closeModal } = useModal();
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleDelete = async (e) => {
+        e.preventDefault();
+        dispatch(listActions.removeComment(commentObj.id));
+        closeModal();
+    };
+
     return (
-        <>
-            <h1>DeleteCommentModal</h1>
-        </>
+        <div id="delete-comment-container">
+            <form id="delete-comment-form">
+                <div>Are you sure you want to delete this comment?</div>
+                <button onClick={handleDelete}>Delete</button>
+                <button onClick={closeModal}>Cancel</button>
+            </form>
+        </div>
     );
 }
 
--- react-app/src/components/EditCommentModal/index.js
+++ react-app/src/components/EditCommentModal/index.js
@@ -1,10 +1,44 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../context/Modal';
+import * as listActions from "../../store/list";
 import './EditComment.css';
 
-function EditCommentModal() {
+function EditCommentModal({ commentObj }) {
+    const { closeModal } = useModal();
+    const dispatch = useDispatch();
+    const [comment, setComment] = useState(commentObj.comment);
+    const [errors, setErrors] = useState([]);
+
+    const handleEdit = async (e) => {
+        e.preventDefault();
+        const data = await dispatch(listActions.editComment(commentObj.id, comment));
+        if (data) {
+            setErrors(data);
+        } else {
+            closeModal();
+        }
+    };
+
     return (
-        <>
-            <h1>EditCommentModal</h1>
-        </>
+        <div id="edit-comment-container">
+            <form id="edit-comment-form">
+                {errors.length > 0 && <ul className="error-message-container">
+                    {errors.map((error, idx) => (
+                        <li className="error-message" key={idx}>{error}</li>
+                    ))}
+                </ul>}
+                <input
+                    type="text"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    placeholder="Add comment here"
+                    required
+                />
+                <button onClick={handleEdit}>Save</button>
+                <button onClick={closeModal}>Cancel</button>
+            </form>
+        </div>
     );
 }
 
