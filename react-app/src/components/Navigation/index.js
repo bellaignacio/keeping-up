71a34ea87024d734870ad79803ebe9fae45dd5a2
Aurora Ignacio <bellaignacio10@gmail.com>
2023-08-02 17:02:18 -0700

    completed logged out nav bar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
 	const history = useHistory();
 	const sessionUser = useSelector(state => state.session.user);
 
@@ -20,16 +20,31 @@ function Navigation({ isLoaded }){
 	// 		)}
 	// 	</ul>
 	// );
-	return (
-		<div id="navigation-container">
-			<div id="keeping-up-icon" onClick={() => history.push('/')}><i className="fas fa-tasks"></i></div>
-			<div id="home-icon" onClick={() => history.push('/')}><i className="fas fa-home"></i></div>
-			<div id="explore-icon" onClick={() => history.push('/explore')}><i className="far fa-compass"></i></div>
-			<div id="create-icon" onClick={() => history.push('/lists/new')}><i className="far fa-plus-square"></i></div>
-			<div id="profile-icon" onClick={() => history.push(`/${sessionUser?.id}`)}><img className="profile-icon-image" src={sessionUser?.image_url} alt={sessionUser?.username} /></div>
-			{isLoaded &&<ProfileButton user={sessionUser} />}
-		</div>
-	);
+	if (sessionUser) {
+		return (
+			<div id="navigation-container">
+				<div id="keeping-up-icon" onClick={() => history.push('/')}><i className="fas fa-tasks"></i></div>
+				<div id="home-icon" onClick={() => history.push('/')}><i className="fas fa-home"></i></div>
+				<div id="explore-icon" onClick={() => history.push('/explore')}><i className="far fa-compass"></i></div>
+				<div id="create-icon" onClick={() => history.push('/lists/new')}><i className="far fa-plus-square"></i></div>
+				<div id="profile-icon" onClick={() => history.push(`/${sessionUser?.id}`)}><img className="profile-icon-image" src={sessionUser?.image_url} alt={sessionUser?.username} /></div>
+				{isLoaded && <ProfileButton user={sessionUser} />}
+			</div>
+		);
+	} else {
+		return (
+			<div id="navigation-container" className="logged-out">
+				<div id="keeping-up-title" onClick={() => history.push('/about')}>
+					<i className="fas fa-tasks"></i>
+					<span>KeepingUp</span>
+				</div>
+				<div id="account-buttons">
+					<button onClick={() => history.push("/login")}>Log in</button>
+					<button onClick={() => history.push("/signup")}>Sign up</button>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Navigation;
