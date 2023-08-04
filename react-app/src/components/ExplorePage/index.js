8dfc598cc22bd4793c9bef4666620f15fb68eeac
Aurora Ignacio <bellaignacio10@gmail.com>
2023-08-03 21:18:26 -0700

    styled no list message

--- react-app/src/components/ExplorePage/index.js
+++ react-app/src/components/ExplorePage/index.js
@@ -31,7 +31,10 @@ function ExplorePage() {
                             return (
                                 <ListTile listOnly={false} listObj={listObj} />
                             );
-                        }) : <div>There are no lists to show. Check again later!</div>}
+                        }) : <div className="no-lists">
+                            <div>All Caught Up!</div>
+                            <div>There are no lists to show.</div>
+                        </div>}
                     </div>
                 }
             </div>
