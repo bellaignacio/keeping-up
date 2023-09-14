bc8319c7281f00d73da9393c5adba4209118e259
Aurora Ignacio <bellaignacio10@gmail.com>
2023-09-14 16:11:37 -0700

    sort search results using levenshteinDistance

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -24,6 +24,50 @@ function Search() {
         );
     });
 
+    function levenshteinDistance(a, b) {
+        // Create a 2D array to store the distances
+        let distances = new Array(a.length + 1);
+        for (let i = 0; i <= a.length; i++) {
+            distances[i] = new Array(b.length + 1);
+        }
+
+        // Initialize the first row and column
+        for (let i = 0; i <= a.length; i++) {
+            distances[i][0] = i;
+        }
+        for (let j = 0; j <= b.length; j++) {
+            distances[0][j] = j;
+        }
+
+        // Fill in the rest of the array
+        for (let i = 1; i <= a.length; i++) {
+            for (let j = 1; j <= b.length; j++) {
+                if (a[i - 1] === b[j - 1]) {
+                    distances[i][j] = distances[i - 1][j - 1];
+                } else {
+                    distances[i][j] = Math.min(distances[i - 1][j], distances[i][j - 1], distances[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+
+        // Return the final distance
+        return distances[a.length][b.length];
+    }
+
+    function sortBySimilarity(objList, singleWord) {
+        // Create an array of objects to store the words and their distances
+        let wordDistances = objList.map(obj => ({
+            object: obj,
+            distance: levenshteinDistance(obj.username || obj.name || obj.title || obj.caption, singleWord)
+        }));
+
+        // Sort the array by distance
+        wordDistances.sort((a, b) => a.distance - b.distance);
+
+        // Return the sorted list of words
+        return wordDistances.map(obj => obj.object);
+    }
+
     return (
         <div id="search-container" className="search-closed">
             <h2 id="search-title">Search</h2>
@@ -31,11 +75,11 @@ function Search() {
                 id="search-input"
                 type="search"
                 value={searchInput}
-                placeholder="Search for lists, users, etc."
+                placeholder="Search for users or lists"
                 onChange={(e) => setSearchInput(e.target.value)}
             />
             {searchInput.length > 0 && <div id="search-result-container">
-                {searchResults.map(obj => {
+                {sortBySimilarity(searchResults, searchInput).map(obj => {
                     return (
                         <div className="search-result" onClick={() => {
                             if (obj.username) history.push(`/${obj.id}`);
